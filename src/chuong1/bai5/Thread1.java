abc76881b2b9917fc8fb405d5af47fbe31dd7ba1
brucelee7 <arigatoubinsan@gmail.com>
2019-09-24 14:14:19 +0700

    Update Sources

--- src/chuong1/bai5/Thread1.java
+++ src/chuong1/bai5/Thread1.java
@@ -1,5 +1,27 @@
 package chuong1.bai5;
 
 public class Thread1 extends Thread{
-    
+    Data data;
+    Thread1(Data data){
+        this.data = data;
+    }
+    @Override
+    public synchronized void run(){
+        for(int i = 2; i <= 1000; i++){
+            if(isPrime(i) == true){
+                this.data.setNumber(i);
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private boolean isPrime(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
 }
--- src/chuong1/bai5/Thread2.java
+++ src/chuong1/bai5/Thread2.java
@@ -1,5 +1,15 @@
 package chuong1.bai5;
 
 public class Thread2 extends Thread {
+    Data data;
+    Thread2(Data data){
+        this.data = data;
+    }
+    public int sum = 0;
+    @Override
+    public synchronized void run(){
+        sum += data.getNumber();
+        notify();
+    }
 
 }
